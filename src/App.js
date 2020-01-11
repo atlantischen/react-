00d916b4ba074a8020e3788ca3af5f9e52390f58
atlantischen <752352055@qq.com>
2020-01-11 15:00:04 +0800

    完成基本配置

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/redux/reducers.js
@@ -0,0 +1,9 @@
+
+
+/**
+ * 用来根据prevState和action生成newState函数模块
+ */
+import { combineReducers } from 'redux';
+export default combineReducers({
+ 
+});
